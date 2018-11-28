671bb8cc8aabf8607c1faec220d3822f65e6f5c7
Robert Paszkowski <robert.paszkowski@wat.edu.pl>
2018-11-28 14:47:30 +0100

    Pierwsza wersja programu, z drobnym błędem. Połączenie z dwóch kont GitHub poprzez VisualStudio

---
+++ KamRaj_test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KamRaj_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("Kamil!!!!, znajdz błąd, wygeneruj następne. ")
+            }
+            Console.ReadKey();
+        }
+    }
+}
